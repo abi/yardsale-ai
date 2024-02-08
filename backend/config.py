c0dda912e2ec1c1278f9d9b9149efbd6ce834d36
Abi Raja <abimanyuraja@gmail.com>
2024-02-07 21:31:14 -0500

    add basic LLM calling

--- backend/config.py
+++ backend/config.py
@@ -3,3 +3,6 @@ import os
 # Set to True when running in production (on the hosted version)
 # Used as a feature flag to enable or disable certain features
 IS_PROD = os.environ.get("IS_PROD", False)
+
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
--- backend/routes/home.py
+++ backend/routes/home.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
+from config import OPENAI_API_KEY
+from llms.core import stream_openai_response
 
 
 router = APIRouter()
@@ -7,6 +9,29 @@ router = APIRouter()
 
 @router.get("/")
 async def get_status():
+    openai_api_key = OPENAI_API_KEY
+    openai_base_url = None
+
+    prompt_messages = [
+        {
+            "role": "system",
+            "content": "Welcome to the Sell Anything app. What would you like to sell today?",
+        }
+    ]
+
+    async def process_chunk(chunk: str):
+        # print(chunk)
+        pass
+
+    completion = await stream_openai_response(
+        prompt_messages,
+        api_key=openai_api_key,
+        base_url=openai_base_url,
+        callback=lambda x: process_chunk(x),
+    )
+
+    print(completion)
+
     return HTMLResponse(
         content="<h3>Your backend is running correctly. Please open the front-end URL (default is http://localhost:5173) to use sell anything.</h3>"
     )
