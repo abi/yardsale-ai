53ee0c6763b76b8005c8e491dfe4f066f67061ff
Abi Raja <abimanyuraja@gmail.com>
2024-02-13 18:34:21 -0500

    include the audio description

--- backend/llms/prompts/analysis.py
+++ backend/llms/prompts/analysis.py
@@ -1,11 +1,14 @@
 SYSTEM_PROMPT = """
-Based on the image, generate a description for the item to sell it on Facebook marketplace.
+Based on the image and the transcribed audio description from the user,
+generate a sales listing for the item to sell on Facebook marketplace.
 
 The format of the response should be as follows:
 
 Title - plain text (up to 150 characters)
 Price - suggest something
-Description - plain text (up to 5000 characters but keep it succint and helpful to the buyer)
+Description - plain text. Up to 5000 characters but keep it succint and helpful to the buyer. 
+Make it informal and friendly. 
+It shouldn't feel too polished and like it was written by a professional copywriter.
 Condition: Supported values: "New"; "Used - Like New"; "Used - Good"; "Used - Fair”
 Category - suggest something
 """
--- backend/routes/home.py
+++ backend/routes/home.py
@@ -15,12 +15,17 @@ async def get_status():
     )
 
 
-def generate_prompt(image_data_url: str):
+def generate_prompt(image_data_url: str, user_description: str):
     user_content: list[ChatCompletionContentPartParam] = [
         {
             "type": "image_url",
             "image_url": {"url": image_data_url, "detail": "high"},
-        }
+        },
+        {
+            "type": "text",
+            "text": "Here is a transcribed audio description from the user:"
+            + user_description,
+        },
     ]
 
     prompt_messages: list[ChatCompletionMessageParam] = [
@@ -47,19 +52,25 @@ async def analyze_item(item: dict[str, str]):
     openai_base_url = None
 
     image_data_url = "https://images.unsplash.com/photo-1581539250439-c96689b516dd?q=80&w=2586&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    prompt_messages = generate_prompt(image_data_url)
+    prompt_messages = generate_prompt(
+        image_data_url,
+        """
+I bought this chair for $300, from Eames. I think it could still go for $100 since it's in good condition. 
+I'm selling it because I'm moving to a new place and I don't have space for it. 
+It's a great chair and I hope someone else can enjoy it as much as I did. 
+I think it's a good deal.
+""",
+    )
 
     async def process_chunk(chunk: str):
         print(chunk, end="", flush=True)
 
-    completion = await stream_openai_response(
+    await stream_openai_response(
         prompt_messages,
         api_key=openai_api_key,
         base_url=openai_base_url,
         callback=lambda x: process_chunk(x),
     )
-
-    print(completion)
 
     # Placeholder for item analysis logic
     # In a real scenario, you would implement the logic to analyze the item details
