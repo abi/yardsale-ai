6416e87bc55f2b9320a87803acf6444faea93258
Abi Raja <abimanyuraja@gmail.com>
2024-02-15 15:23:03 -0500

    add plausible on hosted version

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,29 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
-export default defineConfig({
-  plugins: [react(), checker({ typescript: true })],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [
+      react(),
+      checker({ typescript: true }),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            injectHead: process.env.VITE_IS_HOSTED
+              ? '<script defer="" data-domain="yardsaleai.com" src="https://plausible.io/js/script.tagged-events.outbound-links.js"></script><script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>'
+              : "",
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+  });
+};
