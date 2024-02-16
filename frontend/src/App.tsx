691d891d7dacec0bed64bba778b3ca837c887688
Abi Raja <abimanyuraja@gmail.com>
2024-02-16 10:25:12 -0500

    better initial screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import { CameraView } from "./components/states/CameraView";
 import { ProductDescriptionView } from "./components/states/ProductDescriptionView";
 import { ProcessingView } from "./components/states/ProcessingView";
 import { ResultView } from "./components/states/ResultView";
+import { FaPlay } from "react-icons/fa";
 
 function App() {
   const [logs, setLogs] = useState<string>("");
@@ -99,7 +100,7 @@ function App() {
 
   return (
     <>
-      <div className="w-full xl:w-[1000px] mx-auto mt-2 sm:mt-4 px-4">
+      <div className="w-full xl:w-[1000px] mx-auto mt-2 sm:mt-4 px-4 min-h-screen flex flex-col">
         {/* Navbar */}
         <nav className="border-b border-gray-200 py-2">
           <div className="flex justify-between items-center">
@@ -107,17 +108,37 @@ function App() {
               <img src="/logo.svg" alt="Yardsale AI Logo" className="h-6 w-6" />
               <div className="text-lg font-semibold">Yardsale AI</div>
             </div>
-            <div className="flex items-center space-x-4">
-              <Button variant="secondary" onClick={signIn}>
-                Sign in
-              </Button>
-              <Button onClick={signIn}>Get started</Button>
-            </div>
+            {/* TODO: Only show this on landing page */}
+            {appState === AppState.RESULT && (
+              <div className="flex items-center space-x-4">
+                <Button variant="secondary" onClick={signIn}>
+                  Sign in
+                </Button>
+                <Button onClick={signIn}>Get started</Button>
+              </div>
+            )}
           </div>
         </nav>
 
         {appState === AppState.INITIAL && (
-          <Button onClick={askForPermissions}>Ask for permissions</Button>
+          <div className="flex flex-col flex-1">
+            <h1 className="text-3xl font-bold mt-10 mb-10">
+              Let's craft the perfect Facebook marketplace listing in just a
+              couple of minutes.
+            </h1>
+            <h2 className="text-xl font-semibold mb-10">
+              Step 1: Capture a few pictures of your product <br /> Step 2: Talk
+              about the product <br />
+              Step 3: Your listing is ready!
+            </h2>
+            <Button
+              onClick={askForPermissions}
+              size="lg"
+              className="flex gap-x-2 text-xl"
+            >
+              Start <FaPlay />
+            </Button>
+          </div>
         )}
 
         {appState === AppState.CAMERA && <CameraView />}
--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,14 +1,14 @@
 const Footer = () => {
   return (
-    <footer className="flex justify-between border-t border-gray-200 pt-4 mb-6 px-4 sm:px-0">
+    <footer className="flex flex-col sm:flex-row justify-between border-t border-gray-200 pt-4 mb-6 px-4 sm:px-0">
       <div className="flex flex-col">
         <span className="text-xl mb-2">Yardsale AI</span>
         <span className="text-xs">
           © {new Date().getFullYear()} WhimsyWorks, Inc. All rights reserved.
         </span>
       </div>
-      <div className="flex flex-col text-sm text-gray-600 mr-4">
-        <span className="uppercase">Company</span>
+      <div className="flex flex-wrap gap-x-4 sm:flex-col text-sm text-gray-600 mr-4">
+        {/* <span className="uppercase">Company</span> */}
         <div>WhimsyWorks Inc.</div>
         <div>Made in NYC 🗽</div>
         <a href="https://github.com/abi/yardsale-ai" target="_blank">
